8112014ebb5c7e8ed62b79cd23d86e6ce77da873
itsjofab <jordanlewiss123@gmail.com>
2026-05-26 14:04:52 -0500

    Add PWA update prompt

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from "virtual:pwa-register";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    const shouldUpdate = window.confirm(
+      "A new version of The Booth is available. Refresh now?"
+    );
+
+    if (shouldUpdate) {
+      updateSW(true);
+    }
+  },
+
+  onOfflineReady() {
+    console.log("App ready to work offline.");
+  },
+});
